75cfd3ea187851b2c61c938beef90bb6f6ded9c6
ltisoy <danieltisoy2001@gmail.com>
2021-11-23 16:44:37 -0500

    setting up mongo and mongoose

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 const express = require("express");
-const articleRouter = require("./routes/articles")
+const mongoose = require("mongoose");
+const articleRouter = require("./routes/articles");
 const app = express();
- 
+
+mongoose.connect("mongodb://localhost:27017/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.use("/articles", articleRouter); //articles routes are added after /articles
 
 app.get("/", (req, res) => {
-    const articles = [{
-        title: "Article 1",
-        createdAt: new Date(),
-        description: "This is the first article"
-    }, {
-        title: "Article 2",
-        createdAt: new Date(),
-        description: "This is the second article"
-    }];
-  res.render("index", {articles:articles}); //second parameter is sended to index.ejs
+  const articles = [
+    {
+      title: "Article 1",
+      createdAt: new Date(),
+      description: "This is the first article",
+    },
+    {
+      title: "Article 2",
+      createdAt: new Date(),
+      description: "This is the second article",
+    },
+  ];
+  res.render("articles/index", { articles: articles }); //second parameter is sended to index.ejs
 });
 
 app.listen(5000);
